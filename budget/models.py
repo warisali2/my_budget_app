b256e53b1e3ce6153e79b58223762c1252f7c5ed
Waris Ali <m.warisali2@gmail.com>
2018-02-13 21:42:52 +0500

    feat: Add Expenditure model

--- budget/models.py
+++ budget/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+
+class Expenditure(models.Model):
+    date = models.DateTimeField(primary_key=True)
+    description = models.CharField(max_length=1000)
+    amount = models.DecimalField(decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
