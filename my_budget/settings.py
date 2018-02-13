2900bfbc23c643bdb15f2696ec2a3701b212e3d6
Waris Ali <m.warisali2@gmail.com>
2018-02-13 21:49:50 +0500

    chore: Add budget to INSTALLED_APPS

--- my_budget/settings.py
+++ my_budget/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'budget.apps.BudgetConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
