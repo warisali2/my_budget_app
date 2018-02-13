3700f502818d634ca2751d6a56e85a37ce8377d7
Waris Ali <m.warisali2@gmail.com>
2018-02-13 22:33:56 +0500

    fix: Set column to 'name' for Category List Display

--- budget/admin.py
+++ budget/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Category, Expenditure, IncomeSource
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Expenditure)
 admin.site.register(IncomeSource)
