04b9a46a6463ad68a7d46b4d7ffd4a4370bf64e9
Waris Ali <m.warisali2@gmail.com>
2018-02-13 21:53:35 +0500

    fix: Add max_digits field to DecimalField function

--- budget/models.py
+++ budget/models.py
@@ -6,11 +6,11 @@ class Category(models.Model):
 class Expenditure(models.Model):
     date = models.DateTimeField(primary_key=True)
     description = models.CharField(max_length=1000)
-    amount = models.DecimalField(decimal_places=2)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class IncomeSource(models.Model):
     date = models.DateTimeField(primary_key=True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000, null=True, blank=True)
-    amount = models.DecimalField(decimal_places=2)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
