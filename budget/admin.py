07beeae2cfe3ff85e9e9656b707ab45c673dbfe0
Waris Ali <m.warisali2@gmail.com>
2018-02-13 22:55:11 +0500

    fix: Show category name in Expenditure list display

--- budget/admin.py
+++ budget/admin.py
@@ -5,6 +5,15 @@ from .models import Category, Expenditure, IncomeSource
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
 
+class ExpenditureAdmin(admin.ModelAdmin):
+    list_display = ['date', 'description', 'get_category_name', 'amount']
+
+    def get_category_name(self, obj):
+        return obj.category.name
+
+    get_category_name.admin_order_field = 'category'
+    get_category_name.short_description = 'Name'
+
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Expenditure)
+admin.site.register(Expenditure, ExpenditureAdmin)
 admin.site.register(IncomeSource)
